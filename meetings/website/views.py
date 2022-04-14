37a758dc41790e67cfa87c07047dc0736e672285
Anthony Dominguez <adominguez15722@gmail.com>
2022-04-14 14:26:07 -0500

    made welcome page the homepage

--- meetings/website/views.py
+++ meetings/website/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-
+from datetime import datetime
 # Create your views here.
 
 from django.http import HttpResponse
 
 def welcome(request):
     return HttpResponse('Welcome to the Meeting Planner Application')
+
+def date(request):
+    return HttpResponse(f'This page was served at {str(datetime.now())}')
